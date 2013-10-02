da3dc295be228351181d4c8794292df30ad75e73
Rafael Lima <rafa.cbl@gmail.com>
2013-10-01 23:01:02 -0300

    Add index view

--- web/routers/application_router.ex
+++ web/routers/application_router.ex
@@ -13,7 +13,7 @@ defmodule ApplicationRouter do
   # forward "/posts", to: PostsRouter
 
   get "/" do
-    conn = conn.assign(:title, "Welcome to Dynamo!")
+    conn = conn.assign(:title, "Welcome to Concurrenty Story Teller v0.1!")
     render conn, "index.html"
   end
 end
